35f2e45604f0f8313dfeeb78cf6b1c5213286b5e
ckreisch <ckreisch@wustl.edu>
2017-01-02 00:52:36 -0600

    Added parameter to test. Fixed redundancy in imports.

--- read_input_test.py
+++ read_input_test.py
@@ -1,6 +1,6 @@
-from read_input import read_input
-import re
-from ast import literal_eval as read
+import read_input #from read_input 
+#import re
+#from ast import literal_eval as read
 
 input_file = read_input('input_file')
 param_dic = input_file.param_dic
@@ -9,9 +9,10 @@ lc_path = param_dic['lc_path']
 p0 = param_dic['p0']
 priors = param_dic['priors']
 ndim = param_dic['ndim']
+params = param_dic['transit_parameters']
 
 print lc_path
 print p0
 print priors
 print ndim
-
+print params
