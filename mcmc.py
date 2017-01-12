1ca5b7184b8fa278f587bd5dbd57bed26aea94bd
blacy <blacy@princeton.edu>
2017-01-11 20:40:24 -0500

    fixed bug that occurs if run with 0 burnin

--- mcmc.py
+++ mcmc.py
@@ -47,13 +47,12 @@ class MCMC(object):
         if burnin_steps>0:
             time0 = time.time()
             # burnin phase
-            pos, prob, state  = self._sampler.run_mcmc(p0, burnin_steps)
+            pos, prob, state  = self._sampler.run_mcmc(pos, burnin_steps)
             self._sampler.reset()
             time1=time.time()
             print "burnin time: %f" %(time1-time0)
         elif burnin_steps==0:
             print "no burnin requested"
-            pos=p0
         else:
             print "Warning: incorrect input for burnin steps, setting burnin to 0"
 
