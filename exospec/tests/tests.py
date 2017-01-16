6f324d9088b77beeaa9c348ae5846c77f01ee141
ckreisch <ckreisch@wustl.edu>
2017-01-16 13:33:09 -0500

    test tests

--- exospec/tests/tests.py
+++ exospec/tests/tests.py
@@ -5,7 +5,7 @@ import unittest
 # Find the different unit test scripts and run them
 
 from test_gen_syn_dat import TestSynthData
-from test_mcmc import TestMCMC
+#from test_mcmc import TestMCMC
 #from testTransitModel import TestFunctions
 #from unittest_lc_class import test_lc_class
 #from unittest_read_input import test_read_input
