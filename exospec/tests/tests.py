2736d024615248d990851bb7dd01f7cf11a6edef
ckreisch <ckreisch@wustl.edu>
2017-01-16 13:34:38 -0500

    test tests

--- exospec/tests/tests.py
+++ exospec/tests/tests.py
@@ -6,7 +6,7 @@ import unittest
 
 #from test_gen_syn_dat import TestSynthData
 #from test_mcmc import TestMCMC
-#from testTransitModel import TestFunctions
+from testTransitModel import TestFunctions
 #from unittest_lc_class import test_lc_class
 #from unittest_read_input import test_read_input
 #from test_deliverables import TestLatex
