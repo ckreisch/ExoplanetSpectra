f302261e83f7642dce50e09b857ef68ae9c0467c
Heather Prince <heather.prince1@gmail.com>
2017-01-16 12:46:12 -0500

    Documentation update

--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['differentfilesize',['DifferentFileSize',['../class_different_file_size.html',1,'']]],
   ['differentfilesizes',['DifferentFileSizes',['../classexospec_1_1lc__class_1_1_different_file_sizes.html',1,'exospec::lc_class']]],
   ['differentparamnum',['DifferentParamNum',['../classexospec_1_1lc__class_1_1_different_param_num.html',1,'exospec::lc_class']]]
 ];
