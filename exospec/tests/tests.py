4b1bd5ededbf03038501284fa88ec10e89315acd
ckreisch <ckreisch@wustl.edu>
2017-01-16 13:56:12 -0500

    edit tests

--- exospec/tests/tests.py
+++ exospec/tests/tests.py
@@ -4,13 +4,13 @@ import unittest
 
 # Find the different unit test scripts and run them
 
-#from test_gen_syn_dat import TestSynthData
-#from test_mcmc import TestMCMC
-#from testTransitModel import TestFunctions
-#from unittest_lc_class import test_lc_class
-#from unittest_read_input import test_read_input
+from test_gen_syn_dat import TestSynthData
+from test_mcmc import TestMCMC
+from testTransitModel import TestFunctions
+from unittest_lc_class import test_lc_class
+from unittest_read_input import test_read_input
 from test_deliverables import TestLatex
-#from test_exospec_main import TestIntegration
+from test_exospec_main import TestIntegration
 
 if __name__ == '__main__':
     unittest.main()
