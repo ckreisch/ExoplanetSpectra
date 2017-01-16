b6dfab34380059d4a161bfd8ec55c0cdd3110154
ckreisch <ckreisch@wustl.edu>
2017-01-16 13:37:25 -0500

    test tests

--- exospec/tests/tests.py
+++ exospec/tests/tests.py
@@ -6,10 +6,10 @@ import unittest
 
 #from test_gen_syn_dat import TestSynthData
 #from test_mcmc import TestMCMC
-from testTransitModel import TestFunctions
+#from testTransitModel import TestFunctions
 #from unittest_lc_class import test_lc_class
 #from unittest_read_input import test_read_input
-#from test_deliverables import TestLatex
+from test_deliverables import TestLatex
 #from test_exospec_main import TestIntegration
 
 if __name__ == '__main__':
